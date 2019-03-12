958d3578c7d4870d46b0f9ef3a7d15966496a860
L IN <linzb93@qq.com>
2019-03-12 12:54:29 +0800

    代码多写了

--- server/index.js
+++ server/index.js
@@ -6,8 +6,8 @@ const del = require('del');
 const babel = require('babel-core');
 const browserSync = require('browser-sync').create();
 
-const root = 'src';
-const dist = 'dist';
+const root = 'src'; // 源文件夹
+const dist = 'dist';// 目标文件夹
 
 // 如果没有文件，先创建文件夹
 function writeFile(file, data, callback) {
@@ -83,7 +83,7 @@ function sassRender() {
 // shtml编译成html
 function shtmlCompile(src = root, dest = dist) {
     if (src.split('/').slice(-2) === 'include') {
-        shtmlCompile();
+        // shtmlCompile();
         shtml2Html(root, 'dist', root, () => {
             console.log('shtml编译成功！');
         });
@@ -120,8 +120,6 @@ function babelJsEachCompile(file) {
 
 /**
  * 文件变化时浏览器重载
- * 后面尽量把shtml的include依赖串起来，include文件夹里的文件有修改时，不要所有文件都编译。
- * 其他文件有改动时都编译
  */
 if (process.env.NODE_ENV === 'development') {
     browserSync.init({
